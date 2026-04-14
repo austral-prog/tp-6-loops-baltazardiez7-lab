c55512d7932519de5d65c3edaf4fb6f729eef495
baltazardiez7-lab <baltazardiez7@gmail.com>
2026-04-14 16:57:23 -0300

    Update find_extremes.py

--- find_extremes.py
+++ find_extremes.py
@@ -9,7 +9,7 @@ def find_min(numbers):
     Ejemplo: find_min([5, 5, 5]) -> 5
     Ejemplo: find_min([-3, -1, -7]) -> -7
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    return min(numbers)
 
 
 def find_max(numbers):
@@ -21,7 +21,7 @@ def find_max(numbers):
     Ejemplo: find_max([5, 5, 5]) -> 5
     Ejemplo: find_max([-3, -1, -7]) -> -1
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    return max(numbers)
 
 
 def count_negatives(numbers):
@@ -33,4 +33,10 @@ def count_negatives(numbers):
     Ejemplo: count_negatives([1, 2, 3]) -> 0
     Ejemplo: count_negatives([-1, -2, -3]) -> 3
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    result = 0
+    for num in numbers:
+
+        if num < 0:
+            result+=1
+    return result
+    
